46e3d99a7950c544ddf70271b070c0960f77ac1f
s.ditaranto <simodita99@gmail.com>
2024-02-01 16:26:00 +0100

    mail: se la mail non è presente, chiedo se l'utente vuole registrarla

--- js-mail/js/script.js
+++ js-mail/js/script.js
@@ -8,28 +8,70 @@ const mailList = ["mario.rossi@gmail.com", "mario.verdi@gmail.com", "giorgio.bia
 // memorizzo il bottone 
 const buttonElement = document.querySelector("#check-mail");
 
+// creo un bottone in caso l'utente volesse registrarsi
+const newElement = document.createElement("button");
+
+// memorizzo in una variabile la mail che ha inserito l'utente
+let userMail;
+
+// utilizzo una variabile booleana che per controllare se userMail è presente
+let isMailPresent = false;
+
 buttonElement.addEventListener("click", 
     function() {
-        
-        // memorizzo in una variabile la mail che ha inserito l'utente
-        const userMail = document.getElementById("mail").value;
 
-        // utilizzo una variabile booleana che per controllare se userMail è presente
-        let isMailPresent = false; 
+        // rimuovo il bottone che compare se la mail non esiste e mi voglio registrare
+        newElement.remove();
+
+        const userMailControl = document.getElementById("mail").value;
 
         // controllo nel mio array se userMail è presente 
+        isMailPresent = false;
         for(let i = 0; i < mailList.length; i++) {
-            if(mailList[i] == userMail) {
+            if(mailList[i] == userMailControl) {
                 isMailPresent = true;
             }
         }
-        
+        userMail = userMailControl;
+        console.log(userMail);
+
         if(isMailPresent){
             document.getElementById("result").innerHTML = "Sei registrato!";
             document.getElementById("result").className = "success";
         } else {
             document.getElementById("result").innerHTML = "Mi dispiace ma non sei registrato o la mail inserita non è corretta";
             document.getElementById("result").className = "error";
+
+            
+            newElement.innerHTML = "Vuoi registrarti?";
+            document.querySelector("#register").append(newElement);
         }
+
     }
 )
+
+// bottone che compare nel caso la mail inserita non esiste, chiede all'utente se vuole registrarsi e aggiunge la nuova mail alla lista
+newElement.addEventListener("click",
+    function() {
+
+        // elimino il contenuto del controllo 
+        document.getElementById("result").innerHTML = " ";
+
+        // controllo se la mail che sto inserendo è già esistente, altrimenti aggiungerebbe la nuova mail ad ogni click del bottone
+        for(let i = 0; i < mailList.length; i++) {
+            if(mailList[i] == userMail) {
+                isMailPresent = true;
+            }
+        }
+
+        // se non è presente aggiungo la mail alla mia lista
+        if(!isMailPresent) {
+            mailList.push(userMail);
+        }
+
+        document.getElementById("result").innerHTML = "Registrato con successo!";
+        
+        document.getElementById("result").className = "registered";
+    }
+)
+
