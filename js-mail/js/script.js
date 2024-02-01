f669e13572c5951c93a9333e09247ae65d63a068
s.ditaranto <simodita99@gmail.com>
2024-02-01 15:17:02 +0100

    refactor: cartelle, esercizio mail completato

---
+++ js-mail/js/script.js
@@ -0,0 +1,34 @@
+// Chiedi all’utente la sua email,
+// controlla che sia nella lista di chi può accedere (qui quindi dovremmo creare da noi un array di email casuali),
+// stampa un messaggio appropriato sull’esito del controllo.
+
+// genero un array di email 
+const mailList = ["mario.rossi@gmail.com", "mario.verdi@gmail.com", "giorgio.bianchi@libero.it", "federico.neri@hotmail.com", "andrea.grandi@google.com"];
+
+// memorizzo il bottone 
+const buttonElement = document.querySelector("#check-mail");
+
+buttonElement.addEventListener("click", 
+    function() {
+        
+        // memorizzo in una variabile la mail che ha inserito l'utente
+        const userMail = document.getElementById("mail").value;
+        // console.log(userMail);
+        // utilizzo una variabile booleana che per controllare se userMail è presente
+        let isMailPresent = false; 
+
+
+        // controllo nel mio array se userMail è presente 
+        for(let i = 0; i < mailList.length; i++) {
+            if(mailList[i] == userMail) {
+                isMailPresent = true;
+            }
+        }
+        
+        if(isMailPresent){
+            document.getElementById("result").innerHTML = "Complimenti, sei registrato!";
+        } else {
+            document.getElementById("result").innerHTML = "Mi dispiace ma non sei registrato o la mail inserita non è corretta";
+        }
+    }
+)
